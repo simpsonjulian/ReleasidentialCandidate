48114940d01ac34e09c8cbee11d8f432e0c3a442
Julian Simpson <simpsonjulian@gmail.com>
2012-01-25 21:57:52 +0000

    makes an RC, with source info

--- Rakefile
+++ Rakefile
@@ -1,6 +1,10 @@
-task :default do 
-
+require 'rake/clean'
+CLEAN.include('product/manifest')
+CLEAN.include('product/*.zip')
+task :clobber do 
+  sh " cd product && mvn clean "
+end
+task :default => :clean do 
   sh "giternal update"
   sh "cd product && ./build_rc.sh"
-
 end
--- product/build_rc.sh
+++ product/build_rc.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 workspace=$PWD
 export JAVA_HOME=/usr/lib/jvm/java-6-sun
+rm -rf ~/.m2/repository/org/neo
 for project in *
 do
   if [ -d $project ]; then
     cd $project
-    echo "I am in directory ${project}"
-    [ -f pom.xml -a -d target ] && mvn clean 
     [ -d .git ] && git pull 
-    echo "I am returning to directory ${workspace}"
+    hash=$(git rev-parse HEAD)
+    echo "${project}: ${hash}" >>${workspace}/manifest
     cd $workspace
-  else
-    echo "skipping ${project} as it isn't a directory"
   fi
 done
 export MAVEN_OPTS=-Xmx256m
+set -e
 mvn install -Dlicense.failIfMissing=false 
+packaging="${workspace}/packaging"
+zip -j ${workspace}/neo4j_rc.zip ${workspace}/manifest \
+${packaging}/standalone/target/neo*
